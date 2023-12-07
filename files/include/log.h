2f9be79b00d077b241a2f31f59aaf1f2a9f8d4c9
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2023-12-08 02:25:51 +0300

    update log.h to include new Log Levels

--- files/include/log.h
+++ files/include/log.h
@@ -1,18 +1,24 @@
 #pragma once
 
 #include <ostream>
+#include <sstream>
 #define YELLOW "\033[1;33m"
 #define RED "\033[1;31m"
 #define RESET_COLOR "\033[0m"
 
 #include <fstream>
+#include <iostream>
 #include <string>
-
 enum class LogLevel
 {
+  TRACE,
+  DEBUG,
   INFO,
   WARNING,
-  ERROR
+  ERROR,
+  CRITICAL,
+  EMERGENCY,
+  FATAL
 };
 
 #define WARN LogLevel::WARNING
@@ -29,11 +35,31 @@ class Logger
   void setLogLevel(LogLevel);
   void log(LogLevel, const std::string&);
 
-  Logger& operator<<(const std::string&);
+  template <typename T>
+  Logger& operator<<(const T& value)
+  {
+    if (this->logLevel <= LogLevel::FATAL)
+      {
+        std::ostringstream oss;
+        oss << value;
+        log(this->logLevel, oss.str());
+      }
+    return *this;
+  }
+
+  Logger& operator<<(std::ostream& (*manipulator)(std::ostream&))
+  {
+    manipulator(std::cout);  // Handle manipulators like std::endl
+    // manipulator(oss);
+    // log(this->logLevel, oss.str());
+    return *this;
+  }
+
   Logger& operator()(LogLevel&&);
 
  private:
   std::ofstream outputFile;
+  // std::ostringstream oss;
   LogLevel logLevel;
 
   void writeToLog(const std::string&);
